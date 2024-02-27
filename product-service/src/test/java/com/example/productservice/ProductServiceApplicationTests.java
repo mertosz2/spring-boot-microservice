fb0d7dcc10c0c24d9b4b2105759090317987d1d5
Jonathan Chen <sabza178@gmail.com>
2024-02-27 18:52:43 +0700

    add createProduct test

--- product-service/src/test/java/com/example/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/example/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,67 @@
 package com.example.productservice;
 
+import com.example.productservice.dto.ProductRequest;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+import org.testcontainers.utility.DockerImageName;
 
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Testcontainers
 @SpringBootTest
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private MockMvc mockMvc;
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.0.10");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
     }
+
+    @Test
+    @DisplayName("perform on GET: api/product should return http status 201 and product detail")
+    void createProduct() throws Exception {
+        ProductRequest productRequest = getProductRequest();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String productRequestString = objectMapper.writeValueAsString(productRequest);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(productRequestString))
+                .andExpect(status().isCreated());
+    }
+
+    private ProductRequest getProductRequest() {
+        return ProductRequest.builder()
+                .name("Iphone13")
+                .description("Iphone13")
+                .price(BigDecimal.valueOf(1200))
+                .build();
+    }
+
 
 }
