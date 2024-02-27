ce40f3dd568b3678cf4dd1147622cf237b1d10ba
Jonathan Chen <sabza178@gmail.com>
2024-02-27 21:32:08 +0700

    add OrderRequest, OrderLineItemDto

---
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -0,0 +1,12 @@
+package com.example.orderservice.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+
+}
