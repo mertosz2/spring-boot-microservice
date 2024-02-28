608240d02154fe36d2503481a584b8dcbdcd9e39
Jonathan Chen <sabza178@gmail.com>
2024-02-29 00:35:47 +0700

    Add logic to check product availability before saving order, add WebClientConfig in order-service

--- order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -8,6 +8,7 @@ import com.example.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
@@ -19,16 +20,28 @@ import java.util.stream.Collectors;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    public void placeOrder(OrderRequest orderRequest){
+    private final WebClient webClient;
+
+    public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
-       List<OrderLineItems> orderLineItemsList =  orderRequest.getOrderLineItemDtoList()
+        List<OrderLineItems> orderLineItemsList = orderRequest.getOrderLineItemDtoList()
                 .stream()
                 .map(this::mapToDto).toList();
 
         order.setOrderLineItemsList(orderLineItemsList);
-        orderRepository.save(order);
+        Boolean result = webClient.get()
+                .uri("http://localhost:8082/api/inventory")
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .block();
+        if(result){
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
+
     }
 
     private OrderLineItems mapToDto(OrderLineItemDto orderLineItemDto) {
