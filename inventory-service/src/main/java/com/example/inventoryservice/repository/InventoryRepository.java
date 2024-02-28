f8209bfe8b85cb2f04032b27eaeac0c13cffd82f
Jonathan Chen <sabza178@gmail.com>
2024-02-29 00:54:55 +0700

    Add InventoryResponse class and update isInstock method to return List of InventoryResponse

--- inventory-service/src/main/java/com/example/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/example/inventoryservice/repository/InventoryRepository.java
@@ -5,10 +5,12 @@ import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
-    Optional<Inventory> findBySkuCode(String skuCode);
+
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
