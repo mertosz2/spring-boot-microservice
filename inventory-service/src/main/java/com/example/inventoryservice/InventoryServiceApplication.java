20d76c279da24fbbe4e54e7e432fc88b923e2e20
Jonathan Chen <sabza178@gmail.com>
2024-02-28 15:07:00 +0700

    add findBySkuCode , isInstock Method in InventoryService and applicatuib.yaml

--- inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.example.inventoryservice;
 
+import com.example.inventoryservice.model.Inventory;
+import com.example.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +14,20 @@ public class InventoryServiceApplication {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner loadData(InventoryRepository inventoryRepository){
+        return args -> {
+            Inventory inventory = new Inventory();
+            inventory.setSkuCode("Iphone_13");
+            inventory.setQuantity(100);
+
+            Inventory inventory1 = new Inventory();
+            inventory.setSkuCode("Iphone_13_red");
+            inventory.setQuantity(150);
+
+            inventoryRepository.save(inventory);
+            inventoryRepository.save(inventory1);
+
+        };
+    }
 }
---
+++ inventory-service/src/main/java/com/example/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,14 @@
+package com.example.inventoryservice.repository;
+
+import com.example.inventoryservice.model.Inventory;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    Optional<Inventory> findBySkuCode(String skuCode);
+}
