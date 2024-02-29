6c1f8e8f68d6601f9b3c3d90084ea4a8c69e4f46
Jonathan Chen <sabza178@gmail.com>
2024-03-01 01:22:50 +0700

    Set Eureka client defaultZone to http://localhost:8761/eureka to all module

--- inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -5,9 +5,11 @@ import com.example.inventoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
